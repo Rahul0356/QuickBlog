36c1b6b0ed7669b2e4abebb60db30a42a4a5abb5
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-08-04 07:32:52 +0530

    add the navbar section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Bolg from './pages/Bolg'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+         <Route path='/blog' element={<Bolg/>} />
+      </Routes>
+    </div>
   )
 }
 
