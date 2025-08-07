436f460cdf3a10dd3a1e3ff39bf40fbf3ac6d4b5
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-08-07 07:24:38 +0530

    Add the Sidebar

--- client/src/components/admin/Sidebar.jsx
+++ client/src/components/admin/Sidebar.jsx
@@ -43,7 +43,7 @@ const Sidebar = () => {
   </NavLink>
   <NavLink
     
-    to='/admin/comments'
+    to='/admin/listBlog'
     className={({ isActive }) =>
       `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${
         isActive ? 'bg-primary/10 border-r-4 border-primary' : ''
