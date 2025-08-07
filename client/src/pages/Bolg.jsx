51e81700d95d02acba4a3b146ea0afee91bf1438
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-08-07 07:22:33 +0530

    add the loader

--- client/src/pages/Bolg.jsx
+++ client/src/pages/Bolg.jsx
@@ -4,14 +4,15 @@ import { assets, blog_data, comments_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
 import Moment from 'moment';
 import Footer from '../components/Footer';
+import Loader from '../components/Loader';
 
 const Blog = () => {
   const { id } = useParams();
   const [data, setData] = useState(null);
   const [comments, setComments] = useState([]);
 
-  const [name,setName] = useState('');
-  const [content,setContent] = useState('');
+  const [name, setName] = useState('');
+  const [content, setContent] = useState('');
 
   const fetchBlogData = async () => {
     const blog = blog_data.find((item) => item._id === id);
@@ -21,10 +22,11 @@ const Blog = () => {
   const fetchComments = async () => {
     setComments(comments_data);
   };
-   const addComment = async(e)=>{
-    e.preventDefault();
 
-   }
+  const addComment = async (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
     fetchBlogData();
     fetchComments();
@@ -35,7 +37,7 @@ const Blog = () => {
       <img
         src={assets.gradientBackground}
         alt=""
-        className="absolute -top-50 -z-10 opacity-50"
+        className="absolute -top-32 -z-10 opacity-50"
       />
       <Navbar />
 
@@ -62,8 +64,7 @@ const Blog = () => {
 
         {/* Comments Section */}
         <div className="mt-14 mb-10 max-w-3xl mx-auto">
-          <p className='font-semibold mb-4'>
-            Comments ({comments.length})</p>
+          <p className="font-semibold mb-4">Comments ({comments.length})</p>
           <div className="flex flex-col gap-4">
             {comments.map((item, index) => (
               <div
@@ -77,37 +78,59 @@ const Blog = () => {
                 <p className="text-sm max-w-md ml-8">{item.content}</p>
                 <div className="absolute right-4 bottom-3 flex items-center gap-2 text-xs">
                   {Moment(item.createdAt).fromNow()}
-
                 </div>
               </div>
             ))}
           </div>
         </div>
-        {/*Add Comment Section  */}
-        <div className='max-w-3xl mx-auto'>
-          <p className='font-semibold mb-4'>Add your comment
-          </p>
-          <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
-            <input  onChange={(e)=>setName(e.target.value)}value={name} type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded outline-none'/>
-             <textarea  onChange={(e)=>setContent(e.target.value)}value={content} placeholder='Comment ' className='w-full p-2 border border-gray-300 rounded outline-none h-48 required'></textarea>
-             <button type='submit' className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'>submit</button>
-          </form>
 
+        {/* Add Comment Section */}
+        <div className="max-w-3xl mx-auto">
+          <p className="font-semibold mb-4">Add your comment</p>
+          <form
+            onSubmit={addComment}
+            className="flex flex-col items-start gap-4 max-w-lg"
+          >
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text"
+              placeholder="Name"
+              required
+              className="w-full p-2 border border-gray-300 rounded outline-none"
+            />
+            <textarea
+              onChange={(e) => setContent(e.target.value)}
+              value={content}
+              placeholder="Comment"
+              required
+              className="w-full p-2 border border-gray-300 rounded outline-none h-48"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer"
+            >
+              submit
+            </button>
+          </form>
         </div>
+
         {/* Share Buttons */}
-        <div className='my-24 max-w-3xl mx-auto'>
-<p className='font-semibold my-4'>Share this artcle on social media </p>
-<div className='flex'>
-  <img src={assets.facebook_icon} alt='' width={50} />
-  <img src={assets.twitter_icon} alt='' width={50} />
-  <img src={assets.googleplus_icon} alt='' width={50} />
-</div>
+        <div className="my-24 max-w-3xl mx-auto">
+          <p className="font-semibold my-4">
+            Share this artcle on social media
+          </p>
+          <div className="flex">
+            <img src={assets.facebook_icon} alt="" width={50} />
+            <img src={assets.twitter_icon} alt="" width={50} />
+            <img src={assets.googleplus_icon} alt="" width={50} />
+          </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   ) : (
-    <div>Loading...</div>
+    <Loader />
   );
 };
 
